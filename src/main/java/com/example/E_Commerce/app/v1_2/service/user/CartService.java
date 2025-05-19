46325ea9c4ab0189099ea0b03ae8f81495959e02
Revaz Tutarashvili <140757300+revaztutarashvili@users.noreply.github.com>
2025-05-19 09:54:15 +0400

    Update CartService.java

--- src/main/java/com/example/E_Commerce/app/v1_2/service/user/CartService.java
+++ src/main/java/com/example/E_Commerce/app/v1_2/service/user/CartService.java
@@ -1,7 +1,7 @@
 package com.example.E_Commerce.app.v1_2.service.user;
 import com.example.E_Commerce.app.v1_2.model.CartItem;
 import java.util.Map;
-
+// ეს ინტერფეისი აკონტრაქტებს  პროდუქტების დამატებას/წაშლას კალათიდან 
 public interface CartService {
     void addToCart(String username, String role, String productId, int quantity);
     void removeFromCart(String username, String role, String productId, int quantity);
